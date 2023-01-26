e55b6425b85d51907e09f520b93ae1b2e30ccf41
Tamás Bosánszki <bosanszkitamas@gmail.com>
2023-01-26 13:33:43 +0100

    department model

---
+++ WebAPI/WebAPI/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    public class Department
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
