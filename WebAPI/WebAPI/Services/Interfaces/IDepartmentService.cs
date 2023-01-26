9b8874921118812f9dba915b18f29663ef10e6d9
Tamás Bosánszki <bosanszkitamas@gmail.com>
2023-01-26 13:35:04 +0100

    department service interface

---
+++ WebAPI/WebAPI/Services/Interfaces/IDepartmentService.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI.Models;
+
+namespace WebAPI.Services.Interfaces
+{
+    public interface IDepartmentService
+    {
+        List<Department> GetAllDepartments();
+        void AddDepartment(Department department);
+        void UpdateDepartment(Department department);
+        void DeleteDepartment(long id);
+        List<string> GetDepartmentNames();
+    }
+}
