5a1348f0974227664c1540e355dbb3ea980b81e2
Tamás Bosánszki <bosanszkitamas@gmail.com>
2023-01-26 13:35:12 +0100

    employee service interface

---
+++ WebAPI/WebAPI/Services/Interfaces/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI.Models;
+
+namespace WebAPI.Services.Interfaces
+{
+    public interface IEmployeeService
+    {
+        Employee GetEmployee(long? employeeId);
+        List<Employee> GetAllEmployees();
+        void AddEmployee(Employee employee);
+        void UpdateEmployee(Employee employee);
+        void DeleteEmployee(long id);
+    }
+}
