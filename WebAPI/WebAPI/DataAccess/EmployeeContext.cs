7deec3e32fa95349183c583ca3c2929b8061bc33
Tamás Bosánszki <bosanszkitamas@gmail.com>
2023-01-26 13:33:35 +0100

    database context for entity framework

---
+++ WebAPI/WebAPI/DataAccess/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.DataAccess
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
