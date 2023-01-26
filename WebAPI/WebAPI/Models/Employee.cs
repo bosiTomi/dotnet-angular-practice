0d7077973d913548a46b067bd59236e4fb97f13b
Tamás Bosánszki <bosanszkitamas@gmail.com>
2023-01-26 13:33:49 +0100

    employee model

---
+++ WebAPI/WebAPI/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    public class Employee
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string DepartmentName { get; set; }
+        public string DateOfJoining { get; set; }
+        public string PhotoFileName { get; set; }
+
+    }
+}
